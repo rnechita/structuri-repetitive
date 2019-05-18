a71e099a2aebdba090dca8bb27958bfec5faaadc
rnechita <nechita_roberto@yahoo.com>
2019-05-18 23:01:16 +0300

    Ex18

---
+++ src/optsprezece.java
@@ -0,0 +1,10 @@
+public class optsprezece {
+    public static void main(String[] args){
+        char a='v';
+        if (a == 'v' || a == 'n' || a == 'w' || a == 's'){
+            System.out.println("este punct cardinal");
+        } else {
+            System.out.printf("nu este punct cardinal");
+        }
+    }
+}

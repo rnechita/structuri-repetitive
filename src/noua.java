4f1e215ab017542f3dd26b97efb75f5b56d5cde0
rnechita <nechita_roberto@yahoo.com>
2019-05-18 18:56:47 +0300

    ex9

--- src/noua.java
+++ src/noua.java
@@ -1,7 +1,7 @@
 public class noua {
     public static void main(String[] args) {
         int n = 8;
-            for (int i = 0; i < n; i++) {
+            for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= i; j++) {
                     System.out.print(j + " ");
                 }

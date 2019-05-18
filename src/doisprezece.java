1bed23063d92d94fe7904e21d001da857358f2d6
rnechita <nechita_roberto@yahoo.com>
2019-05-18 19:42:13 +0300

    anotimpuri switch-case

---
+++ src/doisprezece.java
@@ -0,0 +1,30 @@
+public class doisprezece {
+    public static void main(String[] args) {
+        int n = 12;
+        switch(n){
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("primavara");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("vara");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("toamna");
+                break;
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("iarna");
+                break;
+            default:
+                System.out.println("luna invalida");
+        }
+
+    }
+}

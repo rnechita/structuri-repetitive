19ed3d4c108ed135230e65824ee85923a9323b19
rnechita <nechita_roberto@yahoo.com>
2019-05-18 19:51:52 +0300

    Ex13

---
+++ src/treisprezece.java
@@ -0,0 +1,10 @@
+public class treisprezece {
+    public static void main(String[] args){
+        int n = 4;
+        for (int i = 0; i <n ; i++) {
+            System.out.println("go");
+        }
+        System.out.println("gata");
+
+    }
+}

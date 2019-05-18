05718e1b9d164131c79084ff4d7030a5b83efa09
rnechita <nechita_roberto@yahoo.com>
2019-05-18 19:07:26 +0300

    Ex11 v1

---
+++ src/unsprezece.java
@@ -0,0 +1,36 @@
+public class unsprezece {
+    public static void main(String[] args){
+        int n = 9;
+        switch(n){
+            case 1:
+                System.out.println("unu");
+                break;
+            case 2:
+                System.out.println("doi");
+                break;
+            case 3:
+                System.out.println("trei");
+                break;
+            case 4:
+                System.out.println("patru");
+                break;
+            case 5:
+                System.out.println("cinci");
+                break;
+            case 6:
+                System.out.println("sase");
+                break;
+            case 7:
+                System.out.println("sapte");
+                break;
+            case 8:
+                System.out.println("opt");
+                break;
+            case 9:
+                System.out.println("noua");
+                break;
+            default:
+                System.out.println("numar gresit");
+        }
+    }
+}
